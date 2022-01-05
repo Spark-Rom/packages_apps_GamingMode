ca27224ca70dad1491f8a16d9840fee3a69a4910
spkal01 <kalligeross@gmail.com>
2022-01-05 21:18:52 +0200

    GamingMode: Adapt to Spark

--- app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
+++ app/src/main/java/org/exthmui/game/fragment/GamingModeAppListFragment.kt
@@ -18,7 +18,7 @@ package org.exthmui.game.fragment
 
 import android.provider.Settings
 import androidx.preference.PreferenceDataStore
-import com.krypton.settings.preference.SystemSettingsStore
+import com.spark.support.preferences.SystemSettingsStore
 
 import org.exthmui.game.R
 
